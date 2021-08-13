ff0900493324e78d37661c180dbf0aa785066ea8
take <audi.honda.love.car@gmail.com>
2021-08-13 16:43:05 +0900

    循環複雑度を下げてみる

--- src/tmp.py
+++ src/tmp.py
@@ -7,4 +7,39 @@ class TMP:
         pass
 
     def tmp(self):
-        ...
+        for i in range(100):
+            for j in range(200):
+                if i > 20 and i % 2 == 0:
+                    n = i // 2 if j > 10 else i * 2
+                    if n > 10:
+                        ...
+                elif i < 10:
+                    n = j if 50 <= j <= 100 else i
+                    if n > 60:
+                        ...
+                else:
+                    ...
+        for i in range(100):
+            for j in range(200):
+                if i > 20 and i % 2 == 0:
+                    n = i // 2 if j > 10 else i * 2
+                    if n > 10:
+                        ...
+                elif i < 10:
+                    n = j if 50 <= j <= 100 else i
+                    if n > 60:
+                        ...
+                else:
+                    ...
+        for i in range(100):
+            for j in range(200):
+                if i > 20 and i % 2 == 0:
+                    n = i // 2 if j > 10 else i * 2
+                    if n > 10:
+                        ...
+                elif i < 10:
+                    n = j if 50 <= j <= 100 else i
+                    if n > 60:
+                        ...
+                else:
+                    ...
